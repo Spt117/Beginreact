b107b60a3fb052db1d59934fca7e1ff58c3c7fd4
jb <theshitcoinert@gmail.com>
2023-08-29 22:27:21 +0200

    Module 2 Styles OK

--- Module 2/src/exercise/1-style/01.jsx
+++ Module 2/src/exercise/1-style/01.jsx
@@ -1,38 +1,46 @@
-// 🦁 Les styles par défaut pour touts les variants sont :
-// - border: 0
-// - borderRadius: 4
+import styles from './01.module.css';
+import './global.css';
 
-// 🦁 Il y a 3 variants : `primary`, `secondary` et `default`
-// Crée un objet ici qui contient les styles pour chaque variant
-// 💡 const VariantsStyle = { primary: { ... }, secondary: { ... }, default: { ... } }
-// - primary : le background est #646cff
-// - secondary : le background est #16a34a
-// - default : le background est #171717
+const VariantsStyle = {
+  primary: { backgroundColor: '#646cff' },
+  secondary: { backgroundColor: '#16a34a' },
+  default: { backgroundColor: '#171717' },
+};
 
-// 🦁 Il y a deux size : `small` et `large`
-// Fais la même chose que pour les variants (en utilisant SizesStyle)
-// - small : le padding est 8px 16px et le fontSize est 1rem
-// - large : le padding est 12px 24px et le fontSize est 1.2rem
-
-// 💡 `backgroundColor: 'red'` -> On utilise `backgroundColor` et pas `background-color`
-// car on est dans du JavaScript et pas dans du CSS.
+const SizesStyle = {
+  small: { padding: '8px 16px', fontSize: '1rem' },
+  large: { padding: '12px 24px', fontSize: '1.2rem' },
+};
 
 const Button = ({ variant, size, children }) => {
-  // 🦁 Utiliser les objets VariantsStyle et SizesStyle pour appliquer les styles
-  // 💡 variantStyle = VariantsStyle[variant];
+  let variantStyle = VariantsStyle[variant];
+  let sizeStyle = SizesStyle[size];
 
-  // 🦁 Ajouter l'objet `style` sur le bouton
-  // 💡 style={{ ... }}
-  // 💡 Tu pourras utiliser `variantStyle` et `sizeStyle` pour appliquer les styles
-  //    En utilisant le spread operator : { ...variantStyle, ...sizeStyle }
-  return <button>{children}</button>;
+  function test() {
+    return {
+      border: 0,
+      borderRadius: 4,
+      color: 'white',
+    };
+  }
+  return (
+    <button style={{ ...test(), ...variantStyle, ...sizeStyle }}>
+      {children}
+    </button>
+  );
 };
 
 const Demo = () => {
   return (
-    // 🦁 Ajouter des styles pour que les boutons soit alignés au centre avec
-    // display: flex et flexDirection: column ainsi que alignItems: center
-    <div>
+    <div
+      className={styles.button}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 10,
+      }}
+    >
       <Button variant="primary" size="small">
         Primary small
       </Button>
@@ -55,10 +63,8 @@ const Demo = () => {
   );
 };
 
-// ℹ️ Ne pas modifier le code ci-dessous
 const App = () => {
   return (
-    // ℹ️ Cette classe permet d'enlever les styles par défaut des boutons !
     <div className="remove-global-styles">
       <Demo />
     </div>
