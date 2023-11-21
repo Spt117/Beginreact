145c00f537d6d1cc0f64b1bee4764a96d65c0900
jb <theshitcoinert@gmail.com>
2023-11-21 14:26:09 +0100

    useState ok

--- Module 2/src/exercise/4-split-component/04.jsx
+++ Module 2/src/exercise/4-split-component/04.jsx
@@ -4,73 +4,83 @@ import styles from '../../styles/Exercise2.module.css';
 const App = () => {
   return (
     <div className={clsx(styles['container'])}>
-      {/* 🦁 Crée un composant Header avec l'header ci-dessous. */}
-      <header>
-        <h1>Mes recettes de cuisine !</h1>
-        <p>Par BeginReact.dev</p>
-      </header>
+      <Header />
       <hr />
-      {/* 🦁 Crée un composant ShoppingList avec les éléments ci-dessous. */}
-      <div className={clsx(styles['flex-col'])}>
-        <h2>Liste de course</h2>
-        <div className={styles['shopping-list-items']}>
-          <div className={clsx(styles['shopping-item'], styles['bg-paper'])}>
-            <div className={styles.section}>
-              <p>Carotte !</p>
-              <p className={styles.badge}>12</p>
-            </div>
-            <div className={styles.section}>
-              <input type="checkbox" />
-            </div>
-          </div>
-          <div className={clsx(styles['shopping-item'], styles['bg-paper'])}>
-            <div className={styles.section}>
-              <p>Patate !</p>
-              <p className={styles.badge}>11</p>
-            </div>
-            <div className={styles.section}>
-              <input type="checkbox" />
-            </div>
-          </div>
-          <div className={clsx(styles['shopping-item'], styles['bg-paper'])}>
-            <div className={styles.section}>
-              <p>Haricot !</p>
-              <p className={styles.badge}>54</p>
-            </div>
-            <div className={styles.section}>
-              <input type="checkbox" />
-            </div>
-          </div>
-        </div>
-      </div>
+      <ShoppingList />
       <hr />
-      {/* 🦁 Crée un composant Recipes avec les éléments ci-dessous. */}
-      <div className={clsx(styles['flex-col'])}>
-        <h2>Liste de recettes à faire</h2>
-        <ul className={clsx(styles['flex-col'], styles['gap-2'])}>
-          <li className={clsx(styles['flex'], styles['gap-4'])}>
-            <span>Tarte à la carotte</span>
-            <p className={styles.badge}>11.06.22</p>
-          </li>
-
-          <li className={clsx(styles['flex'], styles['gap-4'])}>
-            <span>Purée de patate</span>
-            <p className={styles.badge}>15.08.23</p>
-          </li>
-
-          <li className={clsx(styles['flex'], styles['gap-4'])}>
-            <span>Sauce au haricot</span>
-            <p className={styles.badge}>22.09.24</p>
-          </li>
-        </ul>
-      </div>
+      <Recipes />
       <hr />
-      {/* 🦁 Crée un composant Footer avec le footer ci-dessous. */}
-      <footer>
-        <p>Create by BeginReact.dev</p>
-      </footer>
+      <Footer />
     </div>
   );
 };
 
 export default App;
+
+const Header = () => (
+  <header>
+    <h1>Mes recettes de cuisine !</h1>
+    <p>Par BeginReact.dev</p>
+  </header>
+);
+
+const items = [
+  { name: 'Carotte', quantity: 12 },
+  { name: 'Patate', quantity: 11 },
+  { name: 'Haricot', quantity: 54 },
+];
+
+const ShoppingItem = ({ item, quantity }) => (
+  <div className={clsx(styles['shopping-item'], styles['bg-paper'])}>
+    <div className={styles.section}>
+      <p>{item} !</p>
+      <Style data={quantity} />
+    </div>
+    <div className={styles.section}>
+      <input type="checkbox" />
+    </div>
+  </div>
+);
+
+const ShoppingList = () => (
+  <div className={clsx(styles['flex-col'])}>
+    <h2>Liste de course</h2>
+    <div className={styles['shopping-list-items']}>
+      {items.map((item, index) => (
+        <ShoppingItem item={item.name} quantity={item.quantity} key={index} />
+      ))}
+    </div>
+  </div>
+);
+
+const recipes = [
+  { name: 'Tarte à la carotte', date: '11.06.22' },
+  { name: 'Purée de patate', date: '15.08.23' },
+  { name: 'Sauce au haricot', date: '22.09.24' },
+];
+
+const Recipe = ({ name, date }) => (
+  <li className={clsx(styles['flex'], styles['gap-4'])}>
+    <span>{name}</span>
+    <Style data={date} />
+  </li>
+);
+
+const Recipes = () => (
+  <div className={clsx(styles['flex-col'])}>
+    <h2>Liste de recettes à faire</h2>
+    <ul className={clsx(styles['flex-col'], styles['gap-2'])}>
+      {recipes.map((recipe) => (
+        <Recipe {...recipe} key={recipe.name} />
+      ))}
+    </ul>
+  </div>
+);
+
+const Footer = () => (
+  <footer>
+    <p>Create by BeginReact.dev</p>
+  </footer>
+);
+
+const Style = ({ data }) => <p className={styles.badge}>{data}</p>;
